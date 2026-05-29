9d2b2a162590ff2eb42404b9beaf7a4089f7438d
unknown <204846@stmichaelcollege.nl>
2026-05-29 16:25:17 +0200

    game venster aangemaakt met achtergrond en de basis spel loop

--- game.py
+++ game.py
@@ -1,2 +1,28 @@
 # Space Invaders
 import pygame
+# het spel opstarten
+pygame.init()
+
+# venster aanmaken
+breedte = 600
+hoogte = 800
+scherm = pygame.display.set_mode((breedte, hoogte))
+pygame.display.set_caption("Space Invaders")
+
+# kleuren
+zwart = (0, 0, 0)
+wit = (255, 255, 255)
+
+# spel loop
+draait = True
+while draait:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            draait = False
+    
+    # achtergrond zwart maken
+    scherm.fill(zwart)
+    
+    pygame.display.flip()
+
+pygame.quit()
