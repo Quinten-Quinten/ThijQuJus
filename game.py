8ec473a8825396449d837220167e13d08c4ee1dd
unknown <204846@stmichaelcollege.nl>
2026-05-29 16:56:08 +0200

    in while loop aangepast dat er checked wordt voor beweging dus schip kan nu alle kanten op bewegen

--- game.py
+++ game.py
@@ -15,15 +15,15 @@ wit = (255, 255, 255)
 groen = (0, 255, 0)
 
 # speler
-speler_breedte = 30
-speler_hoogte = 50
-speler_x = breedte // 2 - speler_breedte // 2
-speler_y = hoogte - 60
-speler_snelheid = 5
+schip_breedte = 30
+schip_hoogte = 50
+schip_x = breedte // 2 - schip_breedte // 2
+schip_y = hoogte - 60
+schip_snelheid = 0.12
 
-#dit tekent de spelere op het scherm
-def teken_speler(x, y):
-    pygame.draw.rect(scherm, groen, (x, y, speler_breedte, speler_hoogte))
+#dit tekent de schip op het scherm
+def teken_schip(x, y):
+    pygame.draw.rect(scherm, groen, (x, y, schip_breedte, schip_hoogte))
 
 # de loop voor het spel
 draait = True
@@ -32,8 +32,19 @@ while draait:
         if event.type == pygame.QUIT:
             draait = False
     
+    # toetsen checken of er beweging plaats vindt
+    toetsen = pygame.key.get_pressed()
+    if toetsen[pygame.K_LEFT] and schip_x > 0:
+        schip_x -= schip_snelheid
+    if toetsen[pygame.K_RIGHT] and schip_x < breedte - schip_breedte:
+        schip_x += schip_snelheid
+    if toetsen[pygame.K_UP] and schip_y > 0:
+        schip_y -= schip_snelheid
+    if toetsen[pygame.K_DOWN] and schip_y < hoogte - schip_hoogte:
+        schip_y += schip_snelheid
+
     scherm.fill(zwart)
-    teken_speler(speler_x, speler_y)
+    teken_schip(schip_x, schip_y)
     
     pygame.display.flip()
 
